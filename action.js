48da6223da0936dfaf4e26d1f75efe3672ebc6a7
zgy6677 <guanyu.zhou@mail.utoronto.ca>
2023-04-02 20:15:39 -0400

    adding geojson

--- action.js
+++ action.js
@@ -14,99 +14,99 @@ const map = new mapboxgl.Map({
 map.addControl(new mapboxgl.NavigationControl());
 
 
+/*--------------------------------------------------------------------
+STORE USER INPUT FEATURES AS GEOJSON
+--------------------------------------------------------------------*/
+
+// Create empty GeoJSON objects to hold point features
+let geojson = {
+    'type': 'FeatureCollection',
+    'features': []
+};
+
+//Set data source and style on map load
+map.on('load', () => {
+    //Add datasource using GeoJSON variable
+    map.addSource('inputgeojson', {
+        type: 'geojson',
+        data: geojson
+    });
+
+    //Set style for when new points are added to the data source
+    map.addLayer({
+        'id': 'input-pnts',
+        'type': 'circle',
+        'source': 'inputgeojson',
+        'paint': {
+            'circle-radius': 5,
+            'circle-color': 'blue'
+        }
+    });
+
+});
+
+//Add input features to data source based on mouse click and display in map
+map.on('click', (e) => {
+    //Store clicked point as geojson feature
+    const clickedpoint = {
+        'type': 'Feature',
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [e.lngLat.lng, e.lngLat.lat]
+        }
+    };
+
+    //Add clicked point to previously empty geojson FeatureCollection variable
+    geojson.features.pop(0)
+    geojson.features.push(clickedpoint);
+    //Update the datasource to include clicked points
+    map.getSource('inputgeojson').setData(geojson);
+
+    console.log(geojson.features)
+
+});
+
 // /*--------------------------------------------------------------------
 // STORE USER INPUT FEATURES AS GEOJSON
 // --------------------------------------------------------------------*/
 
-// // Create empty GeoJSON objects to hold point features
-// let geojson = {
-//     'type': 'FeatureCollection',
-//     'features': []
-// };
+document.getElementById('buffbutton').addEventListener('click', () => {
 
-// //Set data source and style on map load
-// map.on('load', () => {
-//     //Add datasource using GeoJSON variable
-//     map.addSource('inputgeojson', {
-//         type: 'geojson',
-//         data: geojson
-//     });
+    //Create empty featurecollection for buffers
+    buffresult = {
+        "type": "FeatureCollection",
+        "features": []
+    };
 
-//     //Set style for when new points are added to the data source
-//     map.addLayer({
-//         'id': 'input-pnts',
-//         'type': 'circle',
-//         'source': 'inputgeojson',
-//         'paint': {
-//             'circle-radius': 5,
-//             'circle-color': 'blue'
-//         }
-//     });
+    //Loop through each point in geojson and use turf buffer function to create 0.5km buffer of input points
+    //Add buffer polygons to buffresult feature collection
+    geojson.features.forEach((feature) => {
+        let buff = turf.buffer(feature, 1); // creating features
+        buffresult.features.pop(0)
+        buffresult.features.push(buff); // adding
+    });
 
-// });
+    map.addSource('buffgeojson', {
+        "type": "geojson",
+        "data": buffresult  //use buffer geojson variable as data source
+    })
 
-// //Add input features to data source based on mouse click and display in map
-// map.on('click', (e) => {
-//     //Store clicked point as geojson feature
-//     const clickedpoint = {
-//         'type': 'Feature',
-//         'geometry': {
-//             'type': 'Point',
-//             'coordinates': [e.lngLat.lng, e.lngLat.lat]
-//         }
-//     };
+    //Show buffers on map using styling
+    map.addLayer({
+        "id": "inputpointbuff",
+        "type": "fill",
+        "source": "buffgeojson",
+        "paint": {
+            'fill-color': "blue",
+            'fill-opacity': 0.5,
+            'fill-outline-color': "black"
+        }
+    });
 
-//     //Add clicked point to previously empty geojson FeatureCollection variable
-//     geojson.features.push(clickedpoint);
-
-//     //Update the datasource to include clicked points
-//     map.getSource('inputgeojson').setData(geojson);
-
-// });
-
-// /*--------------------------------------------------------------------
-// STORE USER INPUT FEATURES AS GEOJSON
-// --------------------------------------------------------------------*/
-
-// document.getElementById('buffbutton').addEventListener('click', () => {
-
-//     //Create empty featurecollection for buffers
-//     buffresult = {
-//         "type": "FeatureCollection",
-//         "features": []
-//     };
-
-//     //Loop through each point in geojson and use turf buffer function to create 0.5km buffer of input points
-//     //Add buffer polygons to buffresult feature collection
-//     geojson.features.forEach((feature) => {
-
-//         //YOUR CODE HERE
-//         //  --> 1. Create variable for buffer and use turf.buffer function
-//         //  --> 2. Use features.push (as in map click event above) to add the buffer feature to the empty featureclass
-
-//     });
-
-//     map.addSource('buffgeojson', {
-//         "type": "geojson",
-//         "data": buffresult  //use buffer geojson variable as data source
-//     })
-
-//     //Show buffers on map using styling
-//     map.addLayer({
-//         "id": "inputpointbuff",
-//         "type": "fill",
-//         "source": "buffgeojson",
-//         "paint": {
-//             'fill-color': "blue",
-//             'fill-opacity': 0.5,
-//             'fill-outline-color': "black"
-//         }
-//     });
-
-//     document.getElementById('buffbutton').disabled = true; //disable  button after click
+    // document.getElementById('buffbutton').disabled = true; //disable  button after click
 
 
-// });
+});
 
 
 
@@ -182,3 +182,12 @@ document.getElementById("boundaryfieldset").addEventListener('change',() => {
 
 
 // add buffer by using turf
+
+
+
+
+
+
+
+// // define buffer, points defined by click event 
+// var buffered = turf.buffer(point, 2, {units: 'kilometers'});
